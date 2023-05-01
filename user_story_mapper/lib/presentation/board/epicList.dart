aa557aca94678532176a4e9052bb5296e73de740
Karik <kl.duszak@gmail.com>
2023-05-01 21:02:34 +0200

    add storry button added

--- user_story_mapper/lib/presentation/board/epicList.dart
+++ user_story_mapper/lib/presentation/board/epicList.dart
@@ -24,41 +24,47 @@ class _EpicList extends State<EpicList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.fromLTRB(15, 5, 15, 10),
-          child: StoryCard(storyData: Story.getEmptyObj2(), color: Colors.red),
-        ),
-        Expanded(
-          child: DragAndDropLists(
-            children: List.generate(
-                _epic.features.length, (index) => _buildList(index)),
-            onItemReorder: _onItemReorder,
-            onListReorder: _onListReorder,
-            axis: Axis.horizontal,
-            listWidth: 215,
-            listDraggingWidth: 215,
-            listPadding: const EdgeInsets.all(5.0),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(15, 5, 15, 10),
+            child:
+                StoryCard(storyData: Story.getEmptyObj2(), color: Colors.red),
           ),
-        ),
-      ],
-    ));
+          Expanded(
+            child: DragAndDropLists(
+              children: List.generate(
+                  _epic.features.length, (index) => _buildList(index)),
+              onItemReorder: _onItemReorder,
+              onListReorder: _onListReorder,
+              axis: Axis.horizontal,
+              listWidth: 215,
+              listDraggingWidth: 215,
+              listPadding: const EdgeInsets.all(5.0),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   _buildList(int outerIndex) {
     var feature = _epic.features[outerIndex];
     return DragAndDropList(
-      leftSide: const VerticalDivider(
-        color: Colors.black,
-        width: 1.5,
-        thickness: 1,
-      ),
-      children: List.generate(feature.stories.length,
-          (index) => _buildItem(feature.stories[index])),
-    );
+        leftSide: const VerticalDivider(
+          color: Colors.black,
+          width: 1.5,
+          thickness: 1,
+        ),
+        footer: Container(
+            alignment: Alignment.center,
+            child: ElevatedButton(onPressed: () {}, child: Text("Add Story"))),
+        children: List.generate(
+          feature.stories.length,
+          (index) => _buildItem(feature.stories[index]),
+        ));
   }
 
   _buildItem(Story item) {
